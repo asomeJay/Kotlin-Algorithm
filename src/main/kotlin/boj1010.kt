13ad1413427ab07e7b8e25a69923da282db4c850
asomejay <museum114@naver.com>
2021-07-21 10:42:25 +0900

    백준 1010

--- src/main/kotlin/boj1010.kt
+++ src/main/kotlin/boj1010.kt
@@ -1,2 +1,18 @@
-class boj1010 {
+
+fun main(args:Array<String>){
+    val testCase:Int = readLine()!!.toInt()
+    for (i in 1..testCase) {
+        val dp = Array(31) { IntArray(31) {0} }
+        for (i in 1..30) {
+            dp[i][1] = i
+            dp[i][0] = 1
+            dp[0][i] = 1
+            dp[i][i] = 1
+        }
+        for (i in 2..30) {
+            for (j in 1 until i) dp[i][j] = dp[i - 1][j - 1] + dp[i-1][j]
+        }
+        val (left, right) = readLine()!!.split(" ").map { it.toInt() }
+        println(dp[right][left])
+    }
 }
